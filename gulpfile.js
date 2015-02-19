f0fbe933a4d5a8a457a333d75274906cd233380d
Michael Norris <mike@michaelnorris.co.uk>
2015-02-19 18:03:16 +0000

    change js output directory

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,19 @@ elixir(function(mix) {
         'resources/assets/sass/bootstrap'
     )
         .copy(
-        'vendor/bower_components/jquery/dist/jquery.min.js',
-        'resources/assets/js/jquery.js'
-    )
-        .copy(
-        'vendor/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
-        'resources/assets/js/bootstrap.js'
-    )
-        .copy(
         'vendor/bower_components/font-awesome-sass/assets/stylesheets/',
         'resources/assets/sass/font-awesome'
     )
         .copy(
         'vendor/bower_components/animate-sass/',
         'resources/assets/sass/animate'
+    )
+        .copy(
+        'vendor/bower_components/jquery/dist/jquery.min.js',
+        'public/js/jquery.js'
+    )
+        .copy(
+        'vendor/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+        'public/js/bootstrap.js'
     );
 });
