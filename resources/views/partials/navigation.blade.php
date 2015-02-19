526141646b7824ebd37f131574b06bb10ceeb614
Michael Norris <mike@michaelnorris.co.uk>
2015-02-19 21:54:58 +0000

    add fixed navbar

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -13,9 +13,9 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li><a href="/home">Home</a></li>
-                @if ( $user )
-                    <li><a href="#">{{ $user->name }}</a></li>
-                @endif
+                {{--@if ( $user )--}}
+                    {{--<li><a href="#">{{ $user->name }}</a></li>--}}
+                {{--@endif--}}
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
